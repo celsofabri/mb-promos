ea62d11f8a01afb733fa3dc616d8cf41bcd47f7d
Celso Fabri Junior <celso.fabri@live.com>
2023-07-19 16:00:04 -0300

    feat: first commit (base structure)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Slider from "react-slick";
+
+import ImgBecks from './img/cerveja-becks.png'
+
+const App = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    className: "center",
+    centerMode: true,
+    centerPadding: "60px",
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <aside className="sidebar">
+        <h1 className="logo">MB Distribuidora</h1>
+      </aside>
+      <section className="promo">
+      <Slider {...settings}>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+        <div className="promo__item">
+          <img src={ImgBecks} alt="Cerveja Becks" />
+        </div>
+      </Slider>
+      </section>
+    </main>
   );
 }
 
